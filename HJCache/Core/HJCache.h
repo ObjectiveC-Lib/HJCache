05fa69cde05ad0289a2eda12b120d500e799faef
navy <lzxy169@gmail.com>
2021-01-08 13:54:08 +0800

    update cache

---
+++ HJCache/Core/HJCache.h
@@ -0,0 +1,31 @@
+//
+//  HJCache.h
+//  HJCache
+//
+//  Created by navy on 2021/1/8.
+//
+
+#ifndef HJCache_h
+#define HJCache_h
+
+//! Project version number for HJCache.
+FOUNDATION_EXPORT double HJCacheVersionNumber;
+//! Project version string for HJCache.
+FOUNDATION_EXPORT const unsigned char HJCacheVersionString[];
+
+#if __has_include(<HJCache/HJCache.h>)
+#import <HJCache/HJDiskCache.h>
+#import <HJCache/HJMemoryCache.h>
+#import <HJCache/HJCacheDefine.h>
+#elif __has_include("HJCache.h")
+#import "HJDiskCache.h"
+#import "HJImageCache.h"
+#import "HJCacheDefine.h"
+#endif
+
+#if __has_include(<HJCache/HJDefaultCache.h>)
+#elif __has_include("HJDefaultCache.h")
+#import "HJDefaultCache.h"
+#endif
+
+#endif /* HJCache_h */
